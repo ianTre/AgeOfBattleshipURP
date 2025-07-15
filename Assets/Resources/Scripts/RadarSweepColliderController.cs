d507bad02c9af16b65de39704deef635f4526fab
Germán Blas García <gerlonesome@hotmail.com>
2025-07-15 15:28:47 -0300

    Radar Beep

--- Assets/Resources/Scripts/RadarSweepColliderController.cs
+++ Assets/Resources/Scripts/RadarSweepColliderController.cs
@@ -2,15 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class RadarSweepColliderController : MonoBehaviour
 {
+    [SerializeField]
+    AudioClip RadarBeepSound;
+    string HitIcon;
+
     void OnTriggerEnter(Collider collider)
     {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = RadarBeepSound;
+        HitIcon = collider.gameObject.name;
+        Debug.Log(HitIcon);
         if (collider.tag == "SweepHit")
+        {
             collider.gameObject.GetComponent<RadarIconFadeoutController>().ResetAlpha();
+
+            if (HitIcon == "HitIcon(Clone)")
+            {
+                collider.gameObject.GetComponent<RadarIconFadeoutController>().ResetAlpha();
+                audio.Play();
+            }
+        }
         //else
-        //    Debug.Log("wrong collider: " + collider.GetInstanceID().ToString() + " - " + collider.name + " - " + collider.tag ?? "");
-    }
+            //    Debug.Log("wrong collider: " + collider.GetInstanceID().ToString() + " - " + collider.name + " - " + collider.tag ?? "");
+        }
 
     private void Update()
     {
